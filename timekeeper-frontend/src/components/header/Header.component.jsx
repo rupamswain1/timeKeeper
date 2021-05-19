aa9f6dd3a5938095db0fe8ee4e64da7a811a9d6d
Rupam <rupam@pop-os.localdomain>
2021-05-20 02:11:19 +0530

    UI changes

--- timekeeper-frontend/src/components/header/Header.component.jsx
+++ timekeeper-frontend/src/components/header/Header.component.jsx
@@ -17,7 +17,7 @@ const Header=()=>{
            
             <div className='optionContainer'>
                     <div className='option'>
-                        SignIn
+                        
                     </div>
                 
                               
